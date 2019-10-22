3defe7e0faa5278818596f8a77074f1bd58c9c52
Robbie Jaeger <jaeger.rob@gmail.com>
2019-10-21 20:01:13 -0600

    Add firebase library and initial login check

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import './App.scss';
+
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
 import Login from '../Login/Login';
 import Main from '../Main/Main';
 
@@ -9,6 +13,29 @@ class App extends Component {
     this.state = {
       isLoggedIn: false
     };
+  }
+
+  componentDidMount() {
+    this.initializeAuth();
+    this.checkIfLoggedIn();
+  }
+
+  initializeAuth = () => {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyBvPeQMobRjWj20SrFuoJUSrMfMyLfdPL4',
+      authDomain: "memento-mori-8609a.firebaseapp.com",
+      projectId: 'memento-mori-8609a'
+    };
+
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  checkIfLoggedIn = () => {
+    if (firebase.auth().currentUser) {
+      this.setState({isLoggedIn: true});
+    } else {
+      this.setState({isLoggedIn: false});
+    }
   }
 
   render() {
