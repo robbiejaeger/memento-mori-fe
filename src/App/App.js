c3f86631652a9250c08a320957f5a42a06f09f42
Robbie Jaeger <jaeger.rob@gmail.com>
2019-10-21 18:49:32 -0600

    Add App boilerplate

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Memento Mori</h1>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    this.state = {
+      isLoggedIn: false;
+    };
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let view;
+
+    if (this.state.loggedIn) {
+      // view = <Login />;
+    } else {
+      // view = <Main />;
+    }
+
+    return (
+      <div className="App">
+        {view}  
+      </div>
+    );
+  }
 }
 
 export default App;
